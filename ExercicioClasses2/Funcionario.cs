2d9f39f0ecdb0b218a0739317145825f4b4d7a2b
Andreia de Moraes <andreiadmrs@gmail.com>
2020-08-22 20:39:29 -0300

    exercicio de introducao a programacao orientada a objetos

---
+++ ExercicioClasses2/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioClasses2
+{
+    class Funcionario
+    {
+        public string nome;
+        public double salario;
+    }
+}
---
+++ ExercicioClasses2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioClasses2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário
+            médio dos funcionários.*/
+            var primeiroFuncionario = new Funcionario();
+            var segundoFuncionario = new Funcionario();
+
+            Console.WriteLine("Digite nome e valor do salário:");
+            primeiroFuncionario.nome = Console.ReadLine();
+            primeiroFuncionario.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite outro nome e valor do salário:");
+            segundoFuncionario.nome = Console.ReadLine();
+            segundoFuncionario.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salarioMedio = (primeiroFuncionario.salario + segundoFuncionario.salario) / 2.0;
+                Console.WriteLine("Sálario médio R$" + salarioMedio.ToString("F2", CultureInfo.InvariantCulture));
+            
+        }
+    }
+}
